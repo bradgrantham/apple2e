3c1017c60b6019d4fb3b10d5e5d9c8a544dd2f28
Brad Grantham <brad.grantham@gmail.com>
2018-08-06 00:11:33 -0700

    implement STZ abs, fix -debugger

--- apple2e.cpp
+++ apple2e.cpp
@@ -1425,7 +1425,7 @@ struct CPU6502
         /* 0x6- */ 6, 6, -1, -1, -1, 3, 5, -1, 4, 2, 2, -1, 5, 4, 6, -1,
         /* 0x7- */ 2, 5, 5, -1, -1, 4, 6, -1, 2, 4, -1, -1, -1, 4, 7, -1,
         /* 0x8- */ 2, 6, -1, -1, 3, 3, 3, -1, 2, -1, 2, -1, 4, 4, 4, -1,
-        /* 0x9- */ 2, 6, 5, -1, 4, 4, 4, -1, 2, 5, 2, -1, -1, 5, -1, -1,
+        /* 0x9- */ 2, 6, 5, -1, 4, 4, 4, -1, 2, 5, 2, -1, 4, 5, -1, -1,
         /* 0xA- */ 2, 6, 2, -1, 3, 3, 3, -1, 2, 2, 2, -1, 4, 4, 4, -1,
         /* 0xB- */ 2, 5, 5, -1, 4, 4, 4, -1, 2, 4, 2, -1, 4, 4, 4, -1,
         /* 0xC- */ 2, 6, -1, -1, 3, 3, 5, -1, 2, 2, 2, -1, 4, 4, 3, -1,
@@ -2681,6 +2681,12 @@ struct CPU6502
                 break;
             }
 
+            case 0x9C: { // STZ abs
+                int addr = read_pc_inc(bus) + read_pc_inc(bus) * 256;
+                bus.write(addr, 0x0);
+                break;
+            }
+
             case 0x8E: { // STX abs
                 int addr = read_pc_inc(bus) + read_pc_inc(bus) * 256;
                 bus.write(addr, x);
@@ -3025,7 +3031,7 @@ int main(int argc, char **argv)
             argv++;
             argc--;
 	} else if(strcmp(argv[0], "-debugger") == 0) {
-            mute = true;
+            debugging = true;
             argv++;
             argc--;
 	} else if(strcmp(argv[0], "-diskII") == 0) {
